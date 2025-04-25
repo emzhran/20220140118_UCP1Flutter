aa81fc7d81991aaf62b86c56e0066fc30de032dc
emzhran <zachran750@gmail.com>
2025-04-25 18:42:01 +0700

    menambahkan fitur show/hide password serta styling pada halaman login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,6 +16,8 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
   }
 
+  bool _isObscure = true;
+
   @override
   Widget build(BuildContext context) {
     final _formKey = GlobalKey<FormState>();
@@ -23,7 +25,10 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF2193b0), Color(0xFF6dd5ed)],
+            colors: [
+              Color.fromARGB(255, 82, 45, 94),
+              Color.fromARGB(255, 111, 20, 72),
+            ],
           ),
         ),
         child: Center(
@@ -33,25 +38,51 @@ class _LoginPageState extends State<LoginPage> {
               key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image(image: AssetImage('./assets/image/AreiLogo.png'),),
+                  Center(
+                    child: Image(
+                      image: AssetImage('./assets/image/AreiLogo.png'),
+                      width: 230,
+                      height: 100,
+                    ),
+                  ),
                   SizedBox(height: 20),
-                  Text(
-                    'SELAMAT DATANG',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
+                  Center(
+                    child: Text(
+                      'SELAMAT DATANG KEMBALI',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                      ),
                     ),
                   ),
                   const SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      "Email",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 4),
                   TextFormField(
                     controller: emailController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: Colors.white),
                       ),
-                      prefixIcon: Icon(Icons.email, color: Colors.white,),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                      prefixIcon: Icon(Icons.email, color: Colors.white),
                       hintText: 'Email',
                       hintStyle: TextStyle(color: Colors.white),
                     ),
@@ -63,15 +94,43 @@ class _LoginPageState extends State<LoginPage> {
                     },
                   ),
                   const SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      "Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 4),
                   TextFormField(
                     controller: passwordController,
-                    obscureText: true,
+                    obscureText: _isObscure,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: Colors.white, width: 4.0),
                       ),
-                      prefixIcon: Icon(Icons.lock, color: Colors.white,),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
                       hintText: 'Password',
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isObscure = !_isObscure;
+                          });
+                        },
+                        icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.white,
+                        ),
+                      ),
                       hintStyle: TextStyle(color: Colors.white),
                     ),
                     validator: (value) {
