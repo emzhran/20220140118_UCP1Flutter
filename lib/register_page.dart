0619d46a2a098c7d2fb73b66c11861e3eaffe12e
emzhran <zachran750@gmail.com>
2025-04-25 21:22:11 +0700

    menambahkan tampilan UI pada halaman register

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -22,6 +22,44 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(184, 171, 165, 211),
+              Color.fromARGB(158, 204, 196, 234),
+            ],
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage('./assets/image/AreiLogo.png'),
+                    width: 230,
+                    height: 100,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'DAFTAR AKUN BARU',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
