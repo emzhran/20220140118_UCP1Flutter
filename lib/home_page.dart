f593f5b9b0daea0b7ba0525fbb5a198061c05429
emzhran <zachran750@gmail.com>
2025-04-27 14:52:11 +0700

    menambahkan appbar pada homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,78 +10,80 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     String displayName = email;
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              color: Color.fromARGB(184, 39, 29, 109),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              child: Row(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(184, 39, 29, 109),
+        toolbarHeight: 120,
+        flexibleSpace: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: const Image(
+                  image: AssetImage('./assets/image/donatello.png'),
+                  width: 55,
+                ),
+              ),
+              const SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 1),
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      child: const Image(
-                        image: AssetImage('./assets/image/donatello.png'),
-                        width: 55,
-                      ),
+                  const Text(
+                    "Selamat Datang",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  Text(
+                    displayName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
-                  ),
-                  const SizedBox(width: 15),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Selamat Datang",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Text(
-                        displayName,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.logout, color: Colors.white),
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                      );
-                    },
                   ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              child: ClipRRect(
-                child: Container(
-                  color: Colors.grey[300],
-                  height: 200,
-                  child: Center(
-                    child: Image(
-                      image: AssetImage('./assets/image/Arei.png'),
-                      fit: BoxFit.cover,
-                      height: 250,
-                    ),
+              const Spacer(),
+              IconButton(
+                icon: const Icon(Icons.logout, color: Colors.white),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          const SizedBox(height: 180),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ClipRRect(
+              child: Container(
+                color: Colors.grey[300],
+                height: 200,
+                width: double.infinity,
+                child: Center(
+                  child: Image(
+                    image: AssetImage('./assets/image/Arei.png'),
+                    fit: BoxFit.cover,
+                    height: 200,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
