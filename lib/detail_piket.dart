b091699b107791980eb57ef4be7eb0c261000a83
emzhran <zachran750@gmail.com>
2025-04-29 11:20:07 +0700

    menambahkan appbar pada detail piket

--- lib/detail_piket.dart
+++ lib/detail_piket.dart
@@ -7,7 +7,7 @@ class DetailPiket extends StatelessWidget {
     super.key,
     required this.piket});
 
-    String _formatTanggal(DateTime tanggal) {
+  String _formatTanggal(DateTime tanggal) {
     String hari = DateFormat('EEEE', 'id_ID').format(tanggal);
     String bulan = DateFormat('MMMM', 'id_ID').format(tanggal);
     return '$hari, ${tanggal.day} $bulan ${tanggal.year}';
@@ -15,6 +15,25 @@ class DetailPiket extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 80,
+        backgroundColor: Color.fromARGB(184, 39, 29, 109),
+        title: Text(
+          'Detail ${piket['tugas']}',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(
+              context, piket
+            );
+          },
+          icon: Icon(Icons.arrow_back,
+          color: Colors.white),
+        ),
+      ),
+    );
   }
 }
