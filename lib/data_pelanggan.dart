be3988e7ec94a36e2dc1d659e817f5861492dd5c
emzhran <zachran750@gmail.com>
2025-04-29 11:55:25 +0700

    membuat halaman data pelanggan dan menambahkan controller

---
+++ lib/data_pelanggan.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DataPelanggan extends StatefulWidget {
+  const DataPelanggan({super.key});
+
+  @override
+  State<DataPelanggan> createState() => _DataPelangganState();
+}
+
+class _DataPelangganState extends State<DataPelanggan> {
+  final TextEditingController namaCustomerController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController noHpController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController kdPosController = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
